05b3ba6e4d88556e0c4db6e4f68975e0506069e2
Chrystopher Allan <71189007+chrys-allan@users.noreply.github.com>
2024-10-03 12:34:16 -0300

    2nd class UML Code

---
+++ Aula2CodeUml/Model/animal.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+class Animal:
+    def __init__(self):
+        self.height = None
+        self.age = None
+        self.height = None
+        self.weight = None
+        self.position = None
+
+    def __init__(self, age, height, weight, position):
+        pass
+
+    def move_x(self, ):
+        pass
+
+    def move_y(self, ):
+        pass
+
+    def move_z(self, ):
+        pass
---
+++ Aula2CodeUml/Model/mammal.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from Animal import Animal
+
+
+class Mammal(Animal):
+    def __init__(self):
+        self.fur = None
+
+    def fur_type(self, ):
+        pass
